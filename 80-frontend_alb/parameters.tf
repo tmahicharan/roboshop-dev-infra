208a12a350742e60058bb68afb9ccc5ee4a2b5c0
tmahicharan <tmahicharan@gmail.com>
2026-08-10 18:25:43 +0530

    infra

--- 80-frontend_alb/parameters.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_ssm_parameter" "aws_lb_listener_frontend_listener" {
-  name  = "/${var.project}/${var.environment}/backend_alb_listener_arn"
-  type  = "String"
-  value = aws_lb_listener.frontend_listener.arn
-}
