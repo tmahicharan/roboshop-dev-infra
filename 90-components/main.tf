a490605f150cea7cb4765c0e77f6cb858ca3e148
tmahicharan <tmahicharan@gmail.com>
2026-08-11 12:26:57 +0530

    infra

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,12 +1,14 @@
-module "components" {
-    for_each = var.components
-    source = "git::https://github.com/tmahicharan/terraform-roboshop-component.git"
-    component= each.key
-    rule_priority= each.value.rule_priority
-    iam_instance_profile = aws_iam_instance_profile.components.name
-}
-
 resource "aws_iam_instance_profile" "components" {
   name = "components"
   role = "EC2SSMParameterRead"
+}
+
+module "components" {
+  for_each = var.components
+
+  source = "git::https://github.com/tmahicharan/terraform-roboshop-component.git"
+
+  component        = each.key
+  rule_priority    = each.value.rule_priority
+  instance_profile = aws_iam_instance_profile.components.name
 }
