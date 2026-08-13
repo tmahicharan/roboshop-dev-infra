bb55018af8362e76bc2bc6408bb928e74471b478
tmahicharan <tmahicharan@gmail.com>
2026-08-13 18:42:36 +0530

    infra

--- 21-vpn/data.tf
+++ 21-vpn/data.tf
@@ -2,12 +2,12 @@ data "aws_ssm_parameter" "openvpn_sg_id" {
   name = "/${var.project}/${var.environment}/openvpn-sg-id"  #/roboshop/dev/openvpn-sg-id
 }
 data "aws_ami" "openvpn" {
-  owners      = ["679593333241"]
+  owners      = ["677673473487"]
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["OpenVPN Access Server Community Image-8fbe3379-*"]
+    values = ["OpenVPN Access Server Community Image-fe8020db-5343-4c43-9e65-5ed4a825c931"]
   }
 
   filter {
